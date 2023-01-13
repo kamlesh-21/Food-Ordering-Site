33df2cac9f9874bd07a6bf028514d9eeb4974fbe
kamlesh-21 <kumar2105k@gmail.com>
2023-01-13 21:01:10 +0530

    updated total element

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
 import { menuArray } from "/data.js"
 // console.log(menuArray)
 
+let orders= []
+document.addEventListener('click', function(e){
+    if(e.target.dataset.add){
+        addItems(e.target.dataset.add)
+    }
+})
+
+function addItems(itemId){
+    
+    const item = menuArray.filter(function(FoodItem){
+        return FoodItem.id.toString() === itemId
+    },)[0]
+    // orders.push(item)
+    // console.log(item)
+    
+    let existingItem = orders.filter(function(order) {
+        return order.name === item.name;
+    });
+    if (existingItem.length > 0) {
+        existingItem[0].quantity += 1;
+    } else {
+        item.quantity = 1;
+        orders.push(item);
+    }
+    updateTotal()
+
+}
+
+function generateTotalHTML() {
+    let total = 0
+        let html = '<div class="total-container">'
+        html += '<h3 class="order-details">Your Order:</h3>'
+        orders.forEach(function(item) {
+            total += item.price * item.quantity
+            html += `<div class="total-item">
+                        <p>${item.name} x ${item.quantity}</p>
+                        <p>${item.price * item.quantity}</p>
+                    </div>`
+        })
+        html += '</div>'
+        html += `<div class="total-cost">
+                    <p>Total Cost:</p>
+                    <p>${total}</p>
+                </div>`
+        html += `<div class="continue">
+                    <button class=gotopayment>Complete Order</button>
+                </div>
+                `
+    return html
+}
+
+function updateTotal(){
+    document.getElementById('total').innerHTML = generateTotalHTML()
+}
+
+
 function getFeed(){
     
     let feedHtml = ''
@@ -13,11 +69,19 @@ function getFeed(){
                    <div>
                         <p class="title">${items.name}</h4>
                         <p class="ingredients">${items.ingredients}</p>
-                        <span class="price"><i class="fa-light fa-dollar-sign"></i>${items.price}</span>
+                        <span class="price">
+                            <i class="fa-regular fa-dollar-sign" 
+                            data-pricing="${items.id}"
+                            ></i> ${items.price}
+                        </span>
                     </div>
                 </div>
                 <div class="add-btn">
-                    <span class="eclipse"><i class="fa-regular fa-plus"></i></span>
+                    <span class="eclipse">
+                        <i class="fa-regular fa-plus" 
+                        data-add="${items.id}"
+                        ></i>
+                    </span>
                 </div>
             </div>
         </div>
